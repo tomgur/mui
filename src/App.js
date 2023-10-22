64deb565eef8643f56820f36e16c368c820c44bb
Tom Gur <tom.gur@zoominfo.com>
2023-10-22 12:55:16 +0300

    feat: add google sso

--- src/App.js
+++ src/App.js
@@ -8,11 +8,66 @@ import Quotes from "./components/Quotes";
 import Bitcoin from "./components/Bitcoin";
 import Settings from "./components/Settings";
 import Calc from "./components/Calc";
+import {googleLogout, useGoogleLogin} from "@react-oauth/google";
+import {useEffect, useState} from "react";
+import axios from 'axios';
 
 const App = () => {
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(
+    () => {
+      if (user) {
+        axios
+          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json'
+            }
+          })
+          .then((res) => {
+            setProfile(res.data);
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [ user ]
+  ); // only re-run the effect if no user is logged in
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
   return (
     <Container maxWidth={"100%"}>
       <Router>
+        <div>
+          <h2>React Google Login</h2>
+          <br />
+          <br />
+
+          {profile ? (
+            <div>
+              <img src={profile.picture} alt="user image" />
+              <h3>User Logged in</h3>
+              <p>Name: {profile.name}</p>
+              <p>Email Address: {profile.email}</p>
+              <br />
+              <br />
+              <button onClick={logOut}>Log out</button>
+            </div>
+          ) : (
+              <button onClick={() => login()}>Sign in with Google 🚀 </button>
+            )}
+
+        </div>
         <div className={"app"}>
           <Stack direction={"row"} justifyContent={"stretch"}>
             <Navbar />
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ThemeProvider} from "@mui/material";
 import theme from "./theme";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </ThemeProvider>
+  <GoogleOAuthProvider clientId={"489467555452-o2fkcperpqrv8uifnbbh4nmv46280vh2.apps.googleusercontent.com"}>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </ThemeProvider>
+  </GoogleOAuthProvider>
 );
